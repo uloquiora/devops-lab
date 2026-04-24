4c3fb07f06255e2fb8a2e82c4d839b19c93f00c3
abdellah <abdellahrahelbusiness@gmail.com>
2026-04-24 09:25:07 +0100

    omar2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 2 - Déployée automatiquement omar"
+    return "Version 2 - Déployée automatiquement omars"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
